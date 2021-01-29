073d854f1bf47cbb77e39d07b2ed74f10783ada4
jorge-pro <jorge.rs.poa@hotmail.com>
2021-01-29 11:08:21 +0100

    App Conversor Moedas - 28/01/2021

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <h1>Conversor de Moedas</h1>
+    <div class="linha">
+      <Conversor moeda-a="USD" moeda-b="BRL"/>
+      <Conversor moeda-a="BRL" moeda-b="USD"/>
+    </div>
+    <div class="linha">
+      <Conversor moeda-a="EUR" moeda-b="BRL"/>
+      <Conversor moeda-a="BRL" moeda-b="EUR"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Conversor from "./components/Conversor.vue";
+export default {
+  name: 'app',
+  components: {
+    Conversor
+  }
+};
+</script>
+
+<style>
+#app {
+  max-width: 900px;
+  margin:auto;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.linha{
+  margin-top:40px;
+  display: flex;
+  justify-content: space-around;
+}
+</style>
